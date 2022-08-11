f99d3f4668a00095873fdbbc222de1677deb75d0
Aladdin54 <91305933+Aladdin54@users.noreply.github.com>
2022-08-11 14:39:23 +1000

    Mach learn algo 2 progressing

--- Backend/mach-learn-algo/mach-learn.py
+++ Backend/mach-learn-algo/mach-learn.py
@@ -4,41 +4,44 @@
 import numpy as np
 import pandas as pd
 import itertools
+
+# Import libraries from scikit learn
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-# Read data from csv file
-data=pd.read_csv('covid-news.csv')
-data.head()
-print(data)
+# Load and read data from csv file
+dataset = pd.read_csv('covid-news.csv')
+dataset.head()
+print(dataset)
 
 # Extract labels from csv file
-lb=data.label
+lb = dataset.label
 lb.head()
 print(lb)
 
-# Split data for training and testing (currently 80:20 - train:test)
-x_train,x_test,y_train,y_test=train_test_split(data['text'], lb, test_size=0.2, random_state=7)
+# Divide data for training and testing (currently 80:20 - train:test)
+x_train, x_test, y_train, y_test = train_test_split(dataset['text'], lb, test_size = 0.2, random_state = 7)
 
-# TFIDF-Vectorizor - text array converted to TF-IDF matrix
-# TF (Term Frequency) - Number of times a work appears in text
-# IDF (Inverse Document Frequency) - measure of how significant a term is in the entire data
-tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
-tfidf_train=tfidf_vectorizer.fit_transform(x_train)
-tfidf_test=tfidf_vectorizer.transform(x_test)
+# TFIDF-Vectorizor - text array converted to TF-IDF matrix to define importance of keyword
+# TF (Term Frequency) - number of times a word appears in text
+# IDF (Inverse Document Frequency) - measure of how significant a work is in the entire data
+tfidf_vectorizer = TfidfVectorizer(stop_words = 'english', max_df = 0.7)
+tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+tfidf_test = tfidf_vectorizer.transform(x_test)
 
-# Passive Agressive Classifier updates loss after each iteration and changes weight vector
-pac=PassiveAggressiveClassifier(max_iter=50)
-pac.fit(tfidf_train,y_train)
+# Passive Agressive Classifier - is an online learning alogorithm which remains passive for a correct classification and turns aggressive for miscalculations.
+# It updates loss after each iteration and changes weight vector
+pac = PassiveAggressiveClassifier(max_iter = 50)
+pac.fit(tfidf_train, y_train)
 
 # Predictions about testing data
-y_pred=pac.predict(tfidf_test)
+y_pred = pac.predict(tfidf_test)
 print(y_pred)
 
 # Calculate accuracy of model over testing data
-score=accuracy_score(y_test,y_pred)
+score = accuracy_score(y_test, y_pred)
 
-print("Accuracy: ",round(score*100,2),"%")
+print("Accuracy: ", round(score*100, 2), "%")
 
---
+++ Backend/mach-learn-algo2/mach-learn2.py
@@ -0,0 +1,76 @@
+# This algorithm currently reads data, conducts some training and testing outputing an accuracy level 
+# Detect if a given news article is fake or real
+#
+# Import modules
+import numpy as np
+import pandas as pd
+import pickle
+
+# Import libraries from scikit learn
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+# from sklearn.featupd.read_csvre_extraction.text import TfidVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import MultinomialNB
+
+
+# Load and read data from csv file
+dataset = pd.read_csv('general-news.csv')
+x = dataset['text']
+y = dataset['label']
+#print(x)
+#print(y)
+dataset.head()
+#print(dataset)
+# Rows and columns
+dataset.shape
+
+# Pre-processing
+
+# Check dataset for null values
+dataset.isnull().any
+
+# Divide data for training and testing (currently 80:20 - train:test)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+# TFIDF-Vectorizor - text array converted to TF-IDF matrix to define importance of keyword
+# TF (Term Frequency) - number of times a word appears in text
+# IDF (Inverse Document Frequency) - measure of how significant a work is in the entire data
+tfidf_vectorizer = TfidfVectorizer(stop_words = 'english', max_df = 0.7)
+tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+tfidf_test = tfidf_vectorizer.transform(x_test)
+
+# Passive Agressive Classifier - is an online learning alogorithm which remains passive for a correct classification and turns aggressive for miscalculations.
+# It updates loss after each iteration and changes weight vector
+pac = PassiveAggressiveClassifier(max_iter = 50)
+pac.fit(tfidf_train, y_train)
+
+# Predictions about testing data
+y_pred = pac.predict(tfidf_test)
+#print(y_pred)
+
+# Calculate accuracy of model over testing data
+score = accuracy_score(y_test, y_pred)
+
+print("Accuracy: ", round(score*100,2), "%")
+
+# Pipeline utility function to train and transform data to text data
+pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words = 'english')), ('nbmodel', MultinomialNB())])
+pipeline.fit(x_train, y_train)
+
+# Calculate accuracy
+score = pipeline.score(x_test, y_test)
+print("Accuracy: ", round(score*100,2), "%")
+
+# Performance evaluation table
+pred = pipeline.predict(x_test)
+print(classification_report(y_test, pred))
+
+# Confusion matrix
+print(confusion_matrix(y_test, pred))
+
+with open('model.pkl', 'wb') as handle:
+    pickle.dump(pipeline, handle, protocol = pickle.HIGHEST_PROTOCOL)
+
