1f3382ed6a1d7d0752cdf2fb0daf36df4042c91c
BVR <15015573+BVRAU@users.noreply.github.com>
2022-09-14 11:07:22 +1000

    Archiving of old DL approach, addition of new approach

---
+++ Backend/zero-shot/train_test/backend_train.py
@@ -0,0 +1,33 @@
+""" backend.py: 
+    
+    This program receives the text input from the Twitter tweet and controls its movement through the backend
+    for preprocessing and other associated computation tasks
+"""
+
+__author__      = "Breydon Verryt-Reid"
+__date__        = "13 Aug 22"
+__Version__     = 1.0
+
+# importing utility scripts
+from os import getcwd
+from preproc_train import *
+from dl_model_train import *
+from svc_layer_train import *
+import pandas as pd
+import os
+
+
+def main(train, test):
+    """ This function controls the flow of the program to the various parts of the backend.
+        
+        ** Parameters **
+        train: name of the tsv containing training Tweets from the Kaggle Dataset
+        test: name of the tsv containing testing/validation Tweets fromn the Kaggle Dataset
+    """
+    preprocmain(train, test)
+    dlmodelmain()
+    #mlmodelmain()
+
+# testing = pd.read_csv('./trg_data/dev.tsv',sep='\t')
+
+main("test.csv", "dev.csv")
